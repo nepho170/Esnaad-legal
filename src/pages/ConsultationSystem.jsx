69859ae6c4ece4ac8d80bfe543f7da4628f56a80
Sara Alhashmi <2lhashmiii@gmail.com>
2025-11-26 14:22:05 +0400

    Updates

--- src/pages/ConsultationSystem.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ConsultationWizard from "../components/consultation/ConsultationWizard";
-
-export default function ConsultationSystem() {
-  return <ConsultationWizard />;
-}
